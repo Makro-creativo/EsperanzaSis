c0cf7c9ba200d7590a5adb82ffa4359b5aa25b26
PaulMaldonado <paul_pastore10@hotmail.com>
2021-11-11 11:55:50 -0600

    Se agrego profile.php para todos los usuarios

---
+++ esperanzasis/profile.php
@@ -0,0 +1,104 @@
+<?php 
+    include "./config/conexion.php";
+
+    session_start();
+    error_reporting(0);
+
+    $typeUser = $_SESSION['Tipo'];
+    $name = $_SESSION['name'];
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EsperanzaSis</title>
+    <link rel="icon" href="assets/img/logo_tortilleria_la_esperanza.svg">
+
+    <!-- Custom fonts for this template-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+    <!-- Custom styles for this page -->
+    <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/profile.css">
+</head>
+<body id="page-top">
+    <div id="wrapper">
+        <?php include "./partials/menuLateral.php" ?>
+
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <?php include "./partials/header.php" ?>
+
+                <div class="container">
+                    <div class="row">
+                        <h2 class="d-flex justify-content-start">Mí perfil</h2>
+
+                        <div class="col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <div class="card shadow-lg">
+                                <div class="card-body">
+                                    <div class="hero-shadow">
+                                        <img src="assets/img/undraw_profile.svg" alt="profile img" class="img-fluid mt-4" style="width: 100%; height: 30vh;">
+                                    </div>
+
+                                    <div class="">
+                                        <h3 class="text-center mt-4"><?php echo $name; ?></h3>
+                                        <h5 class="mt-4 text-dark font-weight-bold text-center"><?php echo $typeUser; ?></h5>
+
+                                        <a href="logout.php" class="btn btn-danger d-flex justify-content-center">Cerrar sesión</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <?php include "./partials/footer.php" ?>
+
+        </div>
+
+    </div>
+
+
+
+
+
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../js/sb-admin-2.min.js"></script>
+
+    <!-- Data tables -->
+    <script src="../vendor/datatables/jquery.dataTables.js"></script>
+    <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="../js/demo/datatables-demo.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="../vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="../js/demo/chart-area-demo.js"></script>
+    <script src="../js/demo/chart-pie-demo.js"></script>
+</body>
+</html>
