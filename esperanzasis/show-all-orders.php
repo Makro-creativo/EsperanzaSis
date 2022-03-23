6db0d263ecfcb8903cffa878ea837f4d7c913699
PaulMaldonado <paul_pastore10@hotmail.com>
2022-03-23 15:30:35 -0600

    Se agrego archivo show-all-orders

--- esperanzasis/show-all-orders.php
+++ esperanzasis/show-all-orders.php
@@ -224,8 +224,26 @@
     <script src="../js/demo/chart-pie-demo.js"></script>
 	
 
-	<script>
+	 <!-- Scripts for buttons for export to excel -->
+	 <script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
+
+    <script>
 		const table = $('#dataTable').DataTable({
+            dom: 'lBfrtip',
+            buttons: [
+                {
+                    extend: 'excel',
+                    text: '<i class="fas fa-file-excel"></i>',
+                    titleAttr: 'EXCEL',
+                    className: 'btn btn-success'
+                }
+            ],
 			language: {
 				"decimal": "",
 				"emptyTable": "No hay información",
