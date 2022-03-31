258f52357bbaa8b98aa6d41c8bf6ea1e582cce90
PaulMaldonado <paul_pastore10@hotmail.com>
2022-03-31 16:31:58 -0600

    Se actualizo el archivo DashboardRepartidor

--- esperanzasis/DashboardRepartidor.php
+++ esperanzasis/DashboardRepartidor.php
@@ -1,3 +1,14 @@
+<?php 
+    session_start();
+
+    if(!isset($_SESSION['contador'])) {
+        $_SESSION['contador'] = 0;
+    } else {
+        $_SESSION['contador']++;
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
