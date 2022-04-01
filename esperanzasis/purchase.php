7889e53d30f0d9bf680fba50946dd33270815b51
PaulMaldonado <paul_pastore10@hotmail.com>
2022-04-01 09:19:03 -0600

    Se actualizo el archivo purchase

--- esperanzasis/purchase.php
+++ esperanzasis/purchase.php
@@ -1,5 +1,6 @@
 <?php
     include "./config/conexion.php";
+
     if(isset($_POST['saveOrder'])){
         $id_user = $_POST['id_user_active'];
         $date_send = $_POST['date_send'];
@@ -9,10 +10,11 @@
 		$calification = $_POST['calification'];
         $statusPaymentClient = $_POST['status_payment_client'];
         $numberNote = $_POST['number_note'];
+        $numberNoteTwo = $_POST['number_note_two'];
 
         $arrayCheck = $_POST['productid'];
         $arrayQuantity = $_POST['quantity'];
-
+        
         $tamanio =  sizeof($arrayCheck);
     
         //INSERTA ORDER
@@ -21,20 +23,34 @@
 		$rowClient = mysqli_fetch_array($query_clients);
 		$nameClient = $rowClient['name_client'];
 		$addressClient = $rowClient['address_company'];
-            echo $query_orders = "INSERT INTO orders (id_user, client_name, address_send, date_send, hour_send, people_order, comments, calification, date_purchase, status_payment_client, number_note) VALUES ('$id_user','$nameClient', '$addressClient', '$date_send', '$hour_send', '$people_order', '$comments', '$calification', NOW(), '$statusPaymentClient', '$numberNote')";
+            $query_orders = "INSERT INTO orders (id_user, client_name, address_send, date_send, hour_send, people_order, comments, calification, date_purchase, status_payment_client, number_note, number_note_two) VALUES ('$id_user','$nameClient', '$addressClient', '$date_send', '$hour_send', '$people_order', '$comments', '$calification', NOW(), '$statusPaymentClient', '$numberNote', '$numberNoteTwo')";
             $result_orders = mysqli_query($conexion, $query_orders);
-            $pid = $conexion->insert_id;
+            
+
+            //$pid = $conexion->insert_id;
+            
             //RECORRRER CHECKBOX CON CANTIDAD DE PRODUCTO
             $total = 0;
             $total_neto = 0;   
-        for ($i = 0; $i < $tamanio; $i++){
-            $array1 = $arrayCheck[$i];
-            $array2 = $arrayQuantity[$i]; 
+            
+        
+        foreach ($arrayQuantity as $key => $value) {
+            if( ($value !="") && ($arrayCheck[$key] !="") ){
+                $array2 = $value;
+                $array1 = $arrayCheck[$key];
+
+                //CONSULT LAST ID ORDER
+                $searchO = "SELECT * FROM orders ORDER BY purchaseid DESC";
+                $query_order = mysqli_query($conexion, $searchO);
+                $row_order = mysqli_fetch_array($query_order);
+                $pid = $row_order['purchaseid'];
+
+            
                 //echo "Valor de check: ".$array1." y el valor de input: ".$array2."<br>";
             $arr = explode("_", $array1);
             $id_product = $arr[0];
             $original_price = $arr[1];
-            $original_price = (float)$original_price;
+            //$original_price = (float)$original_price;
             $quantity = $array2;
             $quantity = (float)$quantity;
 
@@ -49,29 +65,30 @@
             $products_contain_discount = mysqli_num_rows($result_products);
                 
                 if($products_contain_discount > 0) {
-
+                    
                     //Operacion para productos con descuento
                     $row = mysqli_fetch_array($result_products);
                     $apply_discount = $row['discount']; //$5.00
                     $apply_discount = (float)$apply_discount;
                     $discountPartial = $original_price - $apply_discount;//5 por pz
+                    $discountPartial = (float)$discountPartial;
                     $total = $quantity * $discountPartial; //8 * 5 = $40.00
                     //10
                 }else{
 
                     //Operacion para productos sin descuento
-                    $quantity;
                     $total = $original_price * $quantity; 
 
                 }
                 $total = (float)$total;
-                $total_neto += $total; 
+                $total_neto = $total_neto + $total; 
+            }
         }
 
         $query_update_orders = "UPDATE orders SET total='$total_neto' WHERE purchaseid = '$pid'";
-		$result_update_orders = mysqli_query($conexion, $query_update_orders);
+	    $result_update_orders = mysqli_query($conexion, $query_update_orders);
  		
-		//header('location: show-sales.php');	
+		header('location: show-sales.php');	
 
     }else{
 		?>
