0775367e924f9d4edad1becf7d09442f6b9603a5
PaulMaldonado <paul_pastore10@hotmail.com>
2022-07-14 09:58:42 -0500

    Se agrego el archivo reporte de corte super

--- esperanzasis/reports-corte-super.php
+++ esperanzasis/reports-corte-super.php
@@ -1,3 +1,12 @@
+<?php
+    include "./config/conexion.php";
+    
+    $reports_cutbox_super_morning = "SELECT * FROM cutbox_super WHERE turn = 'mañana' ORDER BY opening_date ASC";
+    $result_cutbox_super_morning = mysqli_query($conexion, $reports_cutbox_super_morning);
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -149,6 +158,32 @@
                                             </table>
                                         </div>
                                     </div>
+                                    
+                                    <div class="row mt-2">
+                                        <p class="text-primary text-center">Exportar a excel entre fechas</p>
+                                        <div class="col-md-12 col-sm-12 col-lg-12 col-xl-12 cool-xxl-12">
+                                            <form method="POST" class="form" action="report-cutbox-super-morning.php">
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <input type="date" name="date1" class="form-control">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <input type="date" name="date2" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                
+                                                <div class="d-grid gap-2">
+                                                    <input type="submit" name="generar_reporte" value="Descargar" class="btn btn-success">
+                                                </div>
+                                            </form>
+                                            <br>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -194,5 +229,48 @@
     <!-- Page level custom scripts -->
     <script src="../js/demo/chart-area-demo.js"></script>
     <script src="../js/demo/chart-pie-demo.js"></script>
+
+    <!-- Scripts for buttons for export to excel -->
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
+    <script>
+		const table = $('#dataTable').DataTable({
+            dom: 'lBfrtip',
+            buttons: [
+                {
+                    extend: 'excel',
+                    text: '<i class="fas fa-file-excel"></i>',
+                    titleAttr: 'EXCEL',
+                    className: 'btn btn-success'
+                }
+            ],
+			language: {
+				"decimal": "",
+				"emptyTable": "No hay información",
+				"info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+				"infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+				"infoFiltered": "(Filtrado de _MAX_ total entradas)",
+				"infoPostFix": "",
+				"thousands": ",",
+				"lengthMenu": "Mostrar _MENU_ Entradas",
+				"loadingRecords": "Cargando...",
+				"processing": "Procesando...",
+				"search": "Buscar:",
+				"zeroRecords": "Sin resultados encontrados",
+				"paginate": {
+					"first": "Primero",
+					"last": "Ultimo",
+					"next": "Siguiente",
+					"previous": "Anterior"
+				}
+			},
+			
+		});
+	</script>
 </body>
 </html>
